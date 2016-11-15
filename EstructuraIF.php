dbd03745df813430789e51d6f7b564d35fc5e2d4
Daniel <daniel.s.i@hotmail.com>
2016-11-14 18:22:49 -0600

    Estructuras de control if, else if y else

--- EstructuraIF.php
+++ EstructuraIF.php
@@ -13,12 +13,15 @@ and open the template in the editor.
         <?php
         // Estructura de control if
         $Numero1 = 50;
-        $Numero2 = 20;
+        $Numero2 = 50;
         if ($Numero1 < $Numero2){
             echo $Numero1." es menor que ".$Numero2."<br>";
         }
         elseif($Numero2 < $Numero1){
             echo $Numero2." es menor que ".$Numero1."<br>";
+        }
+        else {
+            echo $Numero1." = ".$Numero2."<br>";
         }
         ?>
     </body>
